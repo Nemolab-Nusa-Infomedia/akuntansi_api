492ab5442eb9a4f8e1a2a60534f8f7a866d72c16
raselsabardila <bautai433@gmail.com>
2024-08-20 16:56:28 +0700

    <dev>(API): Create Model and Migration Notification, LogActivity

---
+++ database/migrations/2024_08_19_062223_create_notifications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->string("id", 100)->primary();
+            $table->string("title", 100);
+            $table->text("body");
+            $table->string("sender_id", 100);
+            $table->string("receiver_id", 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
